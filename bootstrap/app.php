897e2d0b58a5763c327994de82f5d64e11d353f4
tanshadrock0-hub <tan.shadrock0@gmail.com>
2026-05-07 21:38:23 +0800

    add https and proxy config for production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
